de8276eb57e091bc769279f644045a952893ee57
Saim Tariq <saim.trq@eduge.ch>
2024-01-07 20:25:37 +0100

    Ajout du deuxième test "test_affiche_t__t" test reussi

--- test_pendu.py
+++ test_pendu.py
@@ -4,12 +4,13 @@ import unittest
 class TestPendu(unittest.TestCase):
 
     def test_retourne_string(self):
-        jeu_pendu = Pendu()  # Créer une instance de la classe Pendu
+        jeu_pendu = Pendu()
         self.assertIsInstance(jeu_pendu.choisir_mot(), str)
 
-    def test_retourne_string(self):
-        jeu_pendu = Pendu()  # Créer une instance de la classe Pendu
-        self.assertIsInstance(jeu_pendu.afficher_mot_masque(), str)
+    def test_affiche_t__t(self):
+        jeu_pendu = Pendu()
+        lettre_trouvé = ['a', 't', 'c']
+        self.assertEqual(jeu_pendu.afficher_mot_masque('test', lettre_trouvé), 't__t')
 
 if __name__ == '__main__':
     unittest.main()
