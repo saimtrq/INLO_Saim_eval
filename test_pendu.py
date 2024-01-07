f90ed2af9d7e77bba2f24a1713d87d568bc2c495
Saim Tariq <saim.trq@eduge.ch>
2024-01-07 20:40:11 +0100

    Renommage de la deuxième fonction test_retourne_t__t

--- test_pendu.py
+++ test_pendu.py
@@ -7,7 +7,7 @@ class TestPendu(unittest.TestCase):
         jeu_pendu = Pendu()
         self.assertIsInstance(jeu_pendu.choisir_mot(), str)
 
-    def test_affiche_t__t(self):
+    def test_retourne_t__t(self):
         jeu_pendu = Pendu()
         lettre_trouvé = ['a', 't', 'c']
         self.assertEqual(jeu_pendu.afficher_mot_masque('test', lettre_trouvé), 't__t')
